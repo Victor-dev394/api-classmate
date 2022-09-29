3a1ff933af25407fb3bfc689e9f7b58d05382a3c
Victor Araujo Barbosa <victorabarbosa03@gmail.com>
2022-09-28 22:55:13 -0300

    adicionado a classe ProfessorDto para transferência dos dados para a resposta

---
+++ classmate/src/main/java/br/com/victor/classmate/controller/dto/ProfessorDto.java
@@ -0,0 +1,40 @@
+package br.com.victor.classmate.controller.dto;
+
+import org.springframework.data.domain.Page;
+
+import br.com.victor.classmate.model.Professor;
+
+public class ProfessorDto {
+	private String nome;
+	private String email;
+	private String senha;
+	private String telefone;
+	
+	public ProfessorDto(Professor professor) {
+		this.nome = professor.getNome();
+		this.email = professor.getPerfil().getEmail();
+		this.senha = professor.getPerfil().getSenha();
+		this.telefone = professor.getTelefone();
+	}
+	
+	public static Page<ProfessorDto> converter(Page<Professor> professores) {
+		return professores.map(ProfessorDto::new);
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+}
