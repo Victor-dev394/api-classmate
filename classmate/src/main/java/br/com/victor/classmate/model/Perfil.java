bfbf7c22375e8dbe73036e63de032cd2eedb29b1
Victor Araujo Barbosa <victorabarbosa03@gmail.com>
2022-09-28 21:33:28 -0300

    adicionando a classe de modelo Perfil que representa a entidade perfil do banco de dados

---
+++ classmate/src/main/java/br/com/victor/classmate/model/Perfil.java
@@ -0,0 +1,105 @@
+package br.com.victor.classmate.model;
+
+import java.util.Collection;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+public class Perfil implements UserDetails {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String email;
+	private String senha;
+	
+	public Perfil() {
+		
+	}
+
+	public Perfil(String email, String senha) {
+		this.email = email;
+		this.senha = senha;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, id, senha);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Perfil other = (Perfil) obj;
+		return Objects.equals(email, other.email) && Objects.equals(id, other.id) && Objects.equals(senha, other.senha);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+	
+}
