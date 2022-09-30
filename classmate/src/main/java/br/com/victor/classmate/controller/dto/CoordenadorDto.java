6159a18eb684c392c514b1758dffff03cc8ea953
Victor Araujo Barbosa <victorabarbosa03@gmail.com>
2022-09-29 21:05:20 -0300

    adicionando a classe CoordenadorDto para transferência dos dados para a resposta

---
+++ classmate/src/main/java/br/com/victor/classmate/controller/dto/CoordenadorDto.java
@@ -0,0 +1,40 @@
+package br.com.victor.classmate.controller.dto;
+
+import org.springframework.data.domain.Page;
+
+import br.com.victor.classmate.model.Coordenador;
+
+public class CoordenadorDto {
+	private String nome;
+	private String email;
+	private String senha;
+	private String telefone;
+	
+	public CoordenadorDto(Coordenador coordenador) {
+		this.nome = coordenador.getNome();
+		this.email = coordenador.getPerfil().getEmail();
+		this.senha = coordenador.getPerfil().getSenha();
+		this.telefone = coordenador.getTelefone();
+	}
+	
+	public static Page<CoordenadorDto> converter(Page<Coordenador> coordenadores) {
+		return coordenadores.map(CoordenadorDto::new);
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+	
+}
